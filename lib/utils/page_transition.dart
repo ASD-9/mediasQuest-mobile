3ca31e9fa97794b9df98b4333b44f679f8d97458
ASD-9 <ryan.chavatte@gmail.com>
2025-03-19 14:41:01 +0100

    page transition animation

---
+++ lib/utils/page_transition.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PageTransition {
+  static PageRouteBuilder slideTransition({
+    required Widget page,
+    bool fromBottom = false
+  }) {
+    return PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 300),
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = fromBottom ? Offset(0.0, 1.0) : Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        final tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        final offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
+    );
+  }
+}
